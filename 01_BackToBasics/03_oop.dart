140d9e1172b6621500bb25bbcadea357b6a7596a
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2020-09-21 13:34:58 +0530

    Add OOPs

---
+++ 01_BackToBasics/03_oop.dart
@@ -0,0 +1,16 @@
+class TestClass {
+  void display() {
+    print('Hello World');
+  }
+}
+
+void main() {
+  // TestClass tc = new TestClass();
+  var tc = new TestClass();
+  tc.display();
+}
+
+/*
+var is just like in java, where a type is assigned to 
+variable later, when receives first assignment
+*/
