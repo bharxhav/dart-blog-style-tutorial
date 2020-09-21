c3b24ccd762b97f4a104533b20b4e4fe92a27d88
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2020-09-21 13:47:41 +0530

    Add variables

---
+++ 01_BackToBasics/04_data_tyoes.dart
@@ -0,0 +1,15 @@
+import 'dart:ffi';
+
+void main() {
+  // Numbers - Primitive
+  int numa = 10;
+  double deci = 10.0;
+
+  String name = 'Bhargav';
+
+  bool is_bool = true;
+
+  // List and Map too
+
+  // print(numa + deci + name + is_bool);
+}
