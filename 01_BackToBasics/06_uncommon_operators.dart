7284b637117efc2f9a7d4272c3b94e415772a190
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2020-09-24 19:54:34 +0530

    Add null expression

--- 01_BackToBasics/06_uncommon_operators.dart
+++ 01_BackToBasics/06_uncommon_operators.dart
@@ -27,6 +27,32 @@ void main() {
 
   // Null Ternary Operator
   var empty;
+
   empty = empty ?? 10; // Another way of writing ??=
   // But this is used to return a value if an expression on left returns null
+
+  /**
+   * The null ternary operator is very peculior of it's kind :
+   * expr1 ?? expr2
+   * If expr1 is non-null, returns its value;
+   * otherwise, evaluates and returns the value of expr2
+   * 
+   * Basically
+   * e = expr1 ?? expr2
+   * is same as
+   * 
+   * ** In Kotlin
+   *    e = expr1 if expr1 != null else expr2;
+   * 
+   * ** In Dart 
+   *    e = expr1 == null? expr2 : expr1;
+   */
+
+  // Let us say I have an API awaited or a function awaited. I can write
+
+  i_return_null() {
+    return null;
+  }
+
+  var awaited = i_return_null() ?? 0; // Best way to show an application
 }
